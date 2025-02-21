c467073a3c9007deda75df298a796f6261a7af5d
Swaksy7781 <saurabhwakaseseb159@gmail.com>
2025-02-22 00:15:13 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 from PricePredictor import logger
-
-
-logger.info(5)
-
-# from cnnClassifier import logger
-# from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
-# from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+from PricePredictor.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from PricePredictor.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from PricePredictor.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+# from PricePredictor.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
-# STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion stage"
 
 
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     obj = DataIngestionTrainingPipeline()
-#     obj.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 
