44501199b137c54890272e5cb162658c07fd3d66
Swaksy7781 <saurabhwakaseseb159@gmail.com>
2025-02-22 20:33:34 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from PricePredictor import logger
 from PricePredictor.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from PricePredictor.pipeline.stage_02_handling_missing_values import MissingValueHandlingTrainingPipeline # Import new stage
+from PricePredictor.pipeline.stage_02_handling_missing_values import MissingValueHandlingTrainingPipeline
+from PricePredictor.pipeline.stage_03_handling_outliers import OutlierHandlingTrainingPipeline # Import new stage
+
 # from PricePredictor.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 # from PricePredictor.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
-
 STAGE_NAME = "Data Ingestion stage"
-
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -18,11 +18,20 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-
-STAGE_NAME = "Handling Missing Values stage" # Define stage name for new stage
+STAGE_NAME = "Handling Missing Values stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = MissingValueHandlingTrainingPipeline() # Instantiate and run new stage
+    obj = MissingValueHandlingTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Handling Outliers stage" # Define stage name for new stage
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = OutlierHandlingTrainingPipeline() # Instantiate and run new stage
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
